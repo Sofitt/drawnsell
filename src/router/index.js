0eb95c898a1ed5532b7130e21d3e4bd71c0713d3
Sofitt <230320011q@gmail.com>
2022-12-28 03:19:04 +0700

    Remove: Мусор

--- src/router/index.js
+++ src/router/index.js
@@ -6,15 +6,15 @@ const routes = [
     path: '/',
     name: 'draw',
     component: drawPage
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../pages/AboutView.vue')
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../pages/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
