9af7f6de08860898bd4d949d56e09fea5192f9c2
Sofitt <230320011q@gmail.com>
2023-01-23 05:26:46 +0700

    Fix: hamster modal

--- src/components/UI/hamster-modal.vue
+++ src/components/UI/hamster-modal.vue
@@ -59,9 +59,10 @@ export default {
 .hamster-modal {
   z-index: 100;
   position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   display: flex;
   flex-flow: column;
   align-items: center;
